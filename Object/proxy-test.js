da98468843b1ec5ff8423eae467e5fb823fbb295
zlq <midhuhu@163.com>
2023-11-20 09:04:57 +0800

    [mod] 对象代理知识考察 Proxy

--- Object/proxy-test.js
+++ Object/proxy-test.js
@@ -1,10 +1,8 @@
 
 const target = { sum: 0 }
 
-const handler = {
+const add = new Proxy(target, {
     get(target, prop, rec) {
-        // Symbol.toPrimitive 是一个内置的 Symbol 值，
-        // 它是作为对象的函数值属性存在的，当一个对象转换为对应的原始值时，会调用此函数。
         if (prop === Symbol.toPrimitive) {
             let temp = target.sum
             target.sum = 0
@@ -13,8 +11,10 @@ const handler = {
         target.sum += Number(prop)
         return rec
     }
-}
-const add = new Proxy(target, handler)
+})
 
 
-console.log('add', add[1][2][3] + 4)
+console.log('add1', add[1][2][3] + 4)
+console.log('add2', add[10][20] + 30)
+console.log('add3', add[100][200][300] + 400)
+
